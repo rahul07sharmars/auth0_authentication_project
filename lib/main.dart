9e3e118cf873056b975e26d1a68f72fdf9e48f4b
rahul07sharmars <rahul07sharmars@gmail.com>
2023-08-11 14:29:20 +0530

    pub added

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Authoproject',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Auth0'),
+      home: const MyHomePage(title: 'authProject'),
     );
   }
 }
