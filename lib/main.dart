e2e4f5fb2682cf05e2e7b4400c5c9fc97ab2b36d
rahul07sharmars <rahul07sharmars@gmail.com>
2023-08-11 14:42:53 +0530

    urlmismatch

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,54 @@
+import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
+
+import 'profile_view.dart';
+
 //
 void main() {
-  runApp(const MyApp());
+  runApp(const MainView());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainView extends StatefulWidget {
+  const MainView({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  Credentials? _credentials;
+
+  late Auth0 auth0;
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Authoproject',
+      title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'authProject'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Credentials? _credentials;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  late Auth0 auth0;
+
+  @override
+  void initState() {
+    super.initState();
+    auth0 = Auth0('dev-36g1gfwhwyynsncw.us.auth0.com', 'jx7lqQtfRFjp9rW3M5tNgHLAXoPnbnB6');
   }
 
   @override
@@ -45,29 +56,35 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Auth0"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
+      body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            if (_credentials == null)
+              ElevatedButton(
+                  onPressed: () async {
+                    final credentials = await auth0.webAuthentication().login();
+                    setState(() {
+                      _credentials = credentials;
+                    });
+                  },
+                  child: const Text("Log in"))
+            else
+              Column(
+                children: [
+                  ProfileView(user: _credentials!.user),
+                  ElevatedButton(
+                      onPressed: () async {
+                        await auth0.webAuthentication().logout();
+                        setState(() {
+                          _credentials = null;
+                        });
+                      },
+                      child: const Text("Log out"))
+                ],
+              )
+          ]),
     );
   }
 }
