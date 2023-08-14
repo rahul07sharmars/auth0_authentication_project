45331bdd5ca3c0df7e65d3f54a6207b86a302140
rahul07sharmars <rahul07sharmars@gmail.com>
2023-08-14 11:44:20 +0530

    message not found

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:go_router/go_router.dart';
 import 'profile_view.dart';
 
 //
@@ -19,8 +20,6 @@ class _MainViewState extends State<MainView> {
   Credentials? _credentials;
 
   late Auth0 auth0;
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -44,14 +43,35 @@ class _MyHomePageState extends State<MyHomePage> {
   Credentials? _credentials;
 
   late Auth0 auth0;
-  final Auth0_Domain='dev-36g1gfwhwyynsncw.us.auth0.com';
-  final Auth0_Client_ID='LIgkZBqQJKXO3uFNqbgss8NOtHnK8m5C';
-  // final Auth0_Issuer='https://$Auth0_Domain';
+  final FlutterAppAuth appAuth = FlutterAppAuth();
+  final Auth0_Domain = 'dev-36g1gfwhwyynsncw.us.auth0.com';
+  final Auth0_Client_ID = 'Xe0Jzli4IBCAq45ptFfzCnhmYho4vtT5';
+  final Auth0_Issuer = 'https://dev-36g1gfwhwyynsncw.us.auth0.com';
+  final Bundle_ID = 'com.example.auth0_authenication_project';
+  final Auth0_Redirect_URI =
+      'https://dev-36g1gfwhwyynsncw.us.auth0.com/flutter/com.example.auth0_authenication_project/callback';
+  final Referesh_Token_Key='refresh_token';
+
+  // / final Auth0_Issuer='https://$Auth0_Domain';
   @override
   void initState() {
     super.initState();
     auth0 = Auth0(Auth0_Domain, Auth0_Client_ID);
   }
+  bool isLogin=false;
+
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (context, state) =>  MyHomePage(),
+      ),
+      GoRoute(
+        path: '/profile',
+        builder: (context, state) =>  ProfileView(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -63,24 +83,59 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            if (_credentials == null)
+            if (isLogin==false)
               ElevatedButton(
                   onPressed: () async {
-                    final credentials = await auth0.webAuthentication().login();
-                    setState(() {
-                      _credentials = credentials;
-                    });
+                    print("inLogin Button");
+                    final authorizationTokenResponse =
+                        await AuthorizationTokenRequest(
+                            Auth0_Client_ID, Auth0_Redirect_URI,
+                            issuer: Auth0_Issuer,
+                            scopes: [
+                          'openid',
+                          'profile',
+                          'email',
+                          'offline_access'
+                        ]);
+                    print(authorizationTokenResponse);
+                    // AuthorizationRequest(
+                    // try{
+                      final result = await appAuth.authorizeAndExchangeCode(authorizationTokenResponse);
+                      print("result");
+                      print(result);
+                      // print(result?.accessToken);
+                      // print(result?.idToken);
+                      // if(authorizationTokenResponse!=null)
+                      //   setState(() {
+                      //     isLogin=true;
+                      //   });
+                    // }
+                    // on PlatformException{
+                    //   print("authorize_and_exchange_code_failed");
+                    // }
+                    // catch(Exception e){
+                    //   print(e);
+                    // }
+
+                    // final AuthorizationTokenResponse =await appAuth.authorizeAndExchangeCode();
+                    // final credentials = await auth0.webAuthentication().login();
+                    // setState(() {
+                    //   _credentials = credentials;
+                    //
+                    // });
+                    // print(credentials.user);
                   },
                   child: const Text("Log in"))
             else
               Column(
                 children: [
-                  ProfileView(user: _credentials!.user),
+                  ProfileView(),
                   ElevatedButton(
                       onPressed: () async {
                         await auth0.webAuthentication().logout();
                         setState(() {
-                          _credentials = null;
+                          isLogin=false;
+                          // _credentials = null;
                         });
                       },
                       child: const Text("Log out"))
--- lib/profile_view.dart
+++ lib/profile_view.dart
@@ -2,17 +2,19 @@ import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
-  const ProfileView({Key? key, required this.user}) : super(key: key);
+  // const ProfileView({Key? key, required this.user}) : super(key: key);
 
-  final UserProfile user;
+  // final UserProfile user;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        if (user.name != null) Text(user.name!),
-        if (user.email != null) Text(user.email!)
+        Text("Rahul"),
+        Text("Sharma")
+        // if (user.name != null) Text(user.name!),
+        // if (user.email != null) Text(user.email!)
       ],
     );
   }
